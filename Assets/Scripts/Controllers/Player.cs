ea209733f99ff5c60b1461e42cc2f2d719648017
mayuri-290 <sunziting@hotmail.com>
2025-09-09 11:51:46 -0400

    add codes to the players script
    Add codes from class into players script.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -11,14 +11,32 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        float speed = 0.5f;
+
+        Vector2 targetPosition = enemyTransform.position; 
+        Vector2 startPosition = transform.position;
+        Vector2 directionToMove = targetPosition - startPosition;
+
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            transform.position += (Vector3)directionToMove.normalized * speed; //normalized makes the vector a unit vector. 
+        }
+
+
         if (Input.GetKeyDown(KeyCode.B))
         {
-        SpawnBombAtOffset(new Vector3 (0,1));
+            // SpawnBombAtOffset(new Vector3 (0,1));
+
+            Vector2 offset = new Vector2(0f, 1f);
+            SpawnBombAtOffset(offset);
         }
     }
 
-    public void SpawnBombAtOffset(Vector3 inOffSet)
+    public void SpawnBombAtOffset(Vector2 inOffSet)
     {
-        Instantiate(bombPrefab, inOffSet, Quaternion.identity);
+        //Instantiate(bombPrefab, inOffSet, Quaternion.identity);
+        
+        Vector2 spawnPosition = (Vector2)transform.position + inOffSet;
+        Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
     }
 }
