df353a119f2026625171c1637cb14df35de47a86
mayuri-290 <sunziting@hotmail.com>
2025-09-13 15:24:40 -0400

    add task 2 codes
    Add codes for task 2

--- Assets/week 2/InClass&Assignment 2/Scripts/Controllers/Player.cs
+++ Assets/week 2/InClass&Assignment 2/Scripts/Controllers/Player.cs
@@ -12,6 +12,8 @@ public class Player : MonoBehaviour
     public float bombTrailSpacing;
     public int numberOfTrailBombs;
 
+    // List<int> randomPosition = new List<int>();
+
 
 
     void Update()
@@ -29,6 +31,11 @@ public class Player : MonoBehaviour
         {
             SpawnBombTrail(bombTrailSpacing, numberOfTrailBombs);//activate the function here.
         }
+        
+         if (Input.GetKeyDown(KeyCode.M))//press M to spawn a series of bomb.
+        {
+            SpawnBombOnRandomCorner(2f);//activate the function here.
+        }
     }
 
     public void SpawnBombAtOffset(Vector2 inOffSet)
@@ -43,9 +50,43 @@ public class Player : MonoBehaviour
     {
         for (int i = 0; i < numberOfTrailBombs; i++)// use for loop to instantiate a series of bombs. 
         {
-            Vector2 spawnPosition = (Vector2)transform.position + new Vector2(0, -i*bombTrailSpacing-1);//find the correct position to spawn bombs in Y axis. 
+            Vector2 spawnPosition = (Vector2)transform.position + new Vector2(0, -i * bombTrailSpacing - 1);//find the correct position to spawn bombs in Y axis. 
             Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
 
         }
+
+    }
+
+    public void SpawnBombOnRandomCorner(float inDistance)
+    {
+
+        int randomPosition = Random.Range(0, 4);//randomly generate a number from 0 to 4.
+        Vector3 spawnPosition = transform.position;
+        //List<int> randomPosition = new List<int> { 1, 2, 3, 4 };
+
+        if (randomPosition == 0)
+        {
+            Vector3 offset = Vector3.up + Vector3.left;
+            spawnPosition = spawnPosition + offset * inDistance;
+        }
+        if (randomPosition == 1)
+        {
+            Vector3 offset = Vector3.up + Vector3.right;
+            spawnPosition = spawnPosition + offset * inDistance;
+        }
+        if (randomPosition == 2)
+        {
+            Vector3 offset = Vector3.down + Vector3.left;
+            spawnPosition = spawnPosition + offset * inDistance;
+        }
+        if (randomPosition == 3)
+        {
+            Vector3 offset = Vector3.down + Vector3.right;
+            spawnPosition = spawnPosition + offset * inDistance;
+        }
+        Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
+
+
+        //randomPosition.Count = Random.Range(0, randomPosition.Count);
     }
 }
