c2fdf740d57e2183673bd3c9d37f776c4ee91eb5
mayuri-290 <sunziting@hotmail.com>
2025-09-19 20:03:36 -0400

    add task3 codes

--- Assets/week 2/InClass&Assignment 2/Scripts/Controllers/Asteroid.cs
+++ Assets/week 2/InClass&Assignment 2/Scripts/Controllers/Asteroid.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    public float moveSpeed;
+    float moveSpeed = 4f;
     public float arrivalDistance;
-    public float maxFloatDistance;
+    float maxFloatDistance;
+
+    Vector2 direction;
+    Vector2 targetPt;
 
     // Start is called before the first frame update
     void Start()
     {
+        float randomX = Random.Range(0, Screen.width);
+        float randomY = Random.Range(0, Screen.height);
+        targetPt = Camera.main.ScreenToWorldPoint(new Vector2(randomX, randomY));
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        AsteroidMovement();
+    }
+
+    public void AsteroidMovement()
+    {
+        maxFloatDistance = ((Vector3)targetPt - transform.position).magnitude;
+        if (maxFloatDistance > arrivalDistance)
+        {
+            direction = (Vector3)targetPt - transform.position;
+
+            transform.position += (Vector3)direction.normalized * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+        float randomX = Random.Range(0, Screen.width);
+        float randomY = Random.Range(0, Screen.height);
+        targetPt = Camera.main.ScreenToWorldPoint(new Vector2(randomX, randomY));
+        }
+
+        
     }
 }
