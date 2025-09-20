10981f2a05625f43848034f7861cf79e19239ee2
mayuri-290 <sunziting@hotmail.com>
2025-09-20 13:37:58 -0400

    add task 4 codes

--- Assets/week 2/InClass&Assignment 2/Scripts/Controllers/Stars.cs
+++ Assets/week 2/InClass&Assignment 2/Scripts/Controllers/Stars.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Stars : MonoBehaviour
 {
+    int start = 0;
+    int end = 1;
+    float timer = 0;
     public List<Transform> starTransforms;
     public float drawingTime;
-
+    
     // Update is called once per frame
     void Update()
     {
@@ -15,6 +18,24 @@ public class Stars : MonoBehaviour
 
     public void DrawConstellation()
     {
+        if (starTransforms.Count < 2 || drawingTime>0f)
+        {
+            Vector3 a = starTransforms[start].position;
+            Vector3 b = starTransforms[end].position;
 
+            timer += Time.deltaTime / drawingTime;
+            float t = Mathf.Min(timer, 1f);
+
+            Vector3 currentPos = Vector3.Lerp(a, b, t);
+            Debug.DrawLine(a, currentPos, Color.white);
+
+            if (t >= 1f)
+            {
+                start = (start + 1) % starTransforms.Count;
+                end = (start + 1) % starTransforms.Count;
+                timer = 0;
+            }
+        }
+        
     }
 }
