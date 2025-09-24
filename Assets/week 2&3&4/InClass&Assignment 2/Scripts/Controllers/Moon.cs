a39dec534d829244aa550a208707755bb0968028
mayuri-290 <sunziting@hotmail.com>
2025-09-24 01:03:03 -0400

    add code for task 3
    Also modified changes in unity inspector.

--- Assets/week 2&3&4/InClass&Assignment 2/Scripts/Controllers/Moon.cs
+++ Assets/week 2&3&4/InClass&Assignment 2/Scripts/Controllers/Moon.cs
@@ -1,20 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
+    private float radius=3f;
+    public float speed= 30f;
+    private float angle = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(radius, speed, planetTransform);
+    }
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        angle += speed * Time.deltaTime;
+        float radians = angle * Mathf.Deg2Rad;
 
+        float x = Mathf.Cos(radians) * radius;
+        float y = Mathf.Sin(radians) * radius;
+        transform.position = target.position + new Vector3(x, y, 0f);
     }
 }
