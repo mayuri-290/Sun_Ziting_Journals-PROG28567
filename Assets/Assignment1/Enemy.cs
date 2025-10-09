693c63591ab63c844206aaf0a37e013d8eb1ac62
mayuri-290 <sunziting@hotmail.com>
2025-10-08 22:04:52 -0400

    add rotation to assignment 1

--- Assets/Assignment1/Enemy.cs
+++ Assets/Assignment1/Enemy.cs
@@ -4,6 +4,7 @@ public class Enemy : MonoBehaviour
 {
     public Transform playerTransform;
     private float speed = 4f;
+    public float angularSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,11 +15,50 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         EnemyMovement();
+
+        transform.Rotate(0f, 0f, angularSpeed * Time.deltaTime);
+
+        Vector3 directionToTarget = playerTransform.position - transform.position;
+        float dotProduct = Vector3.Dot(transform.right, directionToTarget);
+
+        float currentAngle = Mathf.Rad2Deg * Mathf.Atan2(transform.up.y, transform.up.x);
+        float targetAngle = Mathf.Rad2Deg * Mathf.Atan2(directionToTarget.y, directionToTarget.x);
+
+        float angleRemaining = Mathf.DeltaAngle(currentAngle, targetAngle);
+        float changeInAngle;
+
+        if (dotProduct > 0f)
+        {
+            changeInAngle = -angularSpeed * Time.deltaTime;
+
+            if (changeInAngle < angleRemaining)
+            {
+                transform.Rotate(0f, 0f, angleRemaining);
+            }
+            else
+            {
+                transform.Rotate(0f, 0f, changeInAngle);
+            }
+        }
+        else
+        {
+            changeInAngle = angularSpeed * Time.deltaTime;
+
+            if (changeInAngle > angleRemaining)
+            {
+                transform.Rotate(0f, 0f, angleRemaining);
+            }
+            else
+            {
+                transform.Rotate(0f, 0f, changeInAngle);
+            }
+        }
     }
 
     public void EnemyMovement()
     {
         Vector3 enemyDirection = (playerTransform.position - transform.position).normalized;
         transform.position += enemyDirection * speed * Time.deltaTime;
+
     }
 }
