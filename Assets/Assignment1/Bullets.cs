774d94f98bbf7b840251056d1c0e55f2b0ccd0ae
mayuri-290 <sunziting@hotmail.com>
2025-10-09 22:39:50 -0400

    modified codes in enemy script

--- Assets/Assignment1/Bullets.cs
+++ Assets/Assignment1/Bullets.cs
@@ -1,7 +1,10 @@
+using System.Threading;
 using UnityEngine;
 
 public class Bullets : MonoBehaviour
 {
+    public float speed;
+    public float lifeTime=5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,12 @@ public class Bullets : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += transform.up * speed * Time.deltaTime;
+
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Assignment1/Enemy.cs
+++ Assets/Assignment1/Enemy.cs
@@ -5,6 +5,11 @@ public class Enemy : MonoBehaviour
     public Transform playerTransform;
     private float speed = 4f;
     public float angularSpeed;
+
+    public float shootSpeed = 3f;
+    public GameObject bulletPrefab;
+    public float shootInterval = 1f;
+    public float time = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,6 +20,8 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         EnemyMovement();
+        EnemyRotation();
+        ShootBulletsToPlayer();
     }
 
     public void EnemyMovement()
@@ -22,5 +29,53 @@ public class Enemy : MonoBehaviour
         Vector3 pos = transform.position;
         pos.y = Mathf.Lerp(pos.y, playerTransform.position.y, speed * Time.deltaTime);
         transform.position = pos;
+    }
+
+    public void EnemyRotation()
+    {
+        Vector3 directionToPlayer = playerTransform.position - transform.position;
+        float currentAngle = Mathf.Rad2Deg * Mathf.Atan2(transform.up.y, transform.up.x);
+        float targetAngle = Mathf.Rad2Deg * Mathf.Atan2(directionToPlayer.y, directionToPlayer.x);
+        float angleRemaining = Mathf.DeltaAngle(currentAngle, targetAngle);
+
+        float dotProduct = Vector3.Dot(transform.right, directionToPlayer);
+        float changeInAngle;
+
+        if (dotProduct > 0)
+        {
+            changeInAngle = -angularSpeed * Time.deltaTime;
+            if (changeInAngle < angleRemaining)
+            {
+                transform.Rotate(0f, 0f, angleRemaining);
+            }
+            else
+            {
+                transform.Rotate(0f, 0f, changeInAngle);
+            }
+        }
+        else
+        {
+            changeInAngle = angularSpeed * Time.deltaTime;
+            if (changeInAngle > angleRemaining)
+            {
+                transform.Rotate(0f, 0f, angleRemaining);
+            }
+            else
+            {
+                transform.Rotate(0f, 0f, changeInAngle);
+            }
+        }
+    }
+
+    public void ShootBulletsToPlayer()
+    {
+        time += Time.deltaTime;
+        if (time >= shootInterval)
+        {
+            time = 0f;
+            Vector3 directionToPlayer = (playerTransform.position - transform.position);
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            bullet.transform.up = directionToPlayer;
+        }
     }
 }
