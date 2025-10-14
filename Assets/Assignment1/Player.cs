ad5f9eac30a23c89b480c0a7f92aa596cee3fe5c
mayuri-290 <sunziting@hotmail.com>
2025-10-13 21:51:37 -0400

    add assets to unity

--- Assets/Assignment1/Player.cs
+++ Assets/Assignment1/Player.cs
@@ -3,18 +3,23 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 6f;
+    public float boostSpeed = 10f;
+    public float boostTime = 2f;
+    private float currentSpeed;
+    private float timer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentSpeed = speed;
+        timer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 playerPos = transform.position;
-        playerPos.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        transform.position = playerPos;
+        playerPos.y += Input.GetAxis("Vertical") * currentSpeed * Time.deltaTime;
 
         if(playerPos.y > 4.5f)
         {
@@ -24,6 +29,20 @@ public class Player : MonoBehaviour
         {
             playerPos.y = -4.5f;
         }
-         transform.position = playerPos; 
+        transform.position = playerPos;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentSpeed = boostSpeed;
+            timer = boostTime;
+        }
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                currentSpeed = speed;
+            }
+        }
     }
  }
